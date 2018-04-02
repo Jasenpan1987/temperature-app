7b1c92eb54e6ea4742dd0c9791e566dc37fe477f
Jasenpan1987 <jasenpan@gmail.com>
2018-04-02 16:21:47 +1000

    basic view mapping

--- src/View.js
+++ src/View.js
@@ -6,12 +6,40 @@ const {
   div,
   h1,
   pre,
+  input,
+  select,
+  option
 } = hh(h);
 
+const UNITS = [ "Fahrenheit", "Celsius", "Kelvin" ];
+
+function unitOptions(selectedUnit) {
+  return R.map(
+    unit => option({ value: unit, selected: (selectedUnit === unit) }, unit),
+    UNITS
+  )
+}
+
+function unitSection(dispatch, unit, value) {
+  return div({ className: "w-50 ma1" }, [
+    input({
+      type: "text",
+      className: "db w-100 mv2 pa2 input-reset ba",
+      value,
+    }),
+    select({
+      className: "db w-100 pa2 input-reset br1 bg-white ba b--black",
+    }, unitOptions(unit))
+  ]);
+}
 
 function view(dispatch, model) {
   return div({ className: 'mw6 center' }, [
     h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
+    div({ className: "flex" }, [
+      unitSection(dispatch, model.leftUnit, model.leftValue),
+      unitSection(dispatch, model.rightUnit, model.rightValue)
+    ]),
     pre(JSON.stringify(model, null, 2)),
   ]);
 }
